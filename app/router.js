36a8099232a0756a376bbb9a03c3731e033a2e9a
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2016-08-16 14:12:09 -0500

    router works

--- app/router.js
+++ app/router.js
@@ -7,11 +7,15 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('degenerator', function(){
+
+
   this.route('intro-page');
   this.route('login');
   this.route('register');
   this.route('upload');
   this.route('main');
+  });
 });
 
 export default Router;
