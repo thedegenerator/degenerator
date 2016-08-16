f34f7647ddf14896c7fd618604b23a4070ec0a93
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2016-08-16 14:20:52 -0500

    setup for html mockups

--- app/router.js
+++ app/router.js
@@ -8,13 +8,12 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('degenerator', function(){
-
-
-  this.route('intro-page');
-  this.route('login');
-  this.route('register');
-  this.route('upload');
-  this.route('main');
+    this.route('intro-page');
+    this.route('login');
+    this.route('register');
+    this.route('upload');
+    this.route('main');
+    this.route('degenerate');
   });
 });
 
