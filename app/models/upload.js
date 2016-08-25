515999754eb901b8ddd364f3b31307e720528a9e
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2016-08-25 09:21:43 -0500

    ember-filesystem installed

--- app/models/upload.js
+++ app/models/upload.js
@@ -2,14 +2,13 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title:DS.attr('string'),
-degenThresh: DS.attr('string'),
-addHomepage:DS.attr('boolean'),
-dither:DS.attr('boolean'),
-emboss:DS.attr('boolean'),
-sharpen:DS.attr('boolean'),
-blur:DS.attr('boolean'),
-implode:DS.attr('boolean'),
-cycle:DS.attr('boolean'),
-uploadFile:DS.attr('string')
+  degenThresh: DS.attr('string'),
+  dither:DS.attr('boolean'),
+  emboss:DS.attr('boolean'),
+  sharpen:DS.attr('boolean'),
+  blur:DS.attr('boolean'),
+  implode:DS.attr('boolean'),
+  cycle:DS.attr('boolean'),
+  uploadFile:DS.attr('string')
 
 });
