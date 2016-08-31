1ffa17eb060e79e0cac5521d004441003aede9a7
Morgan Higby-Flowers <mhigbyflowers@gmail.com>
2016-08-31 17:30:47 -0500

    comments

--- app/degenerator/degenerate/controller.js
+++ app/degenerator/degenerate/controller.js
@@ -1,5 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+actions:{
+  postComment(upload,formValues, reset){
+    this.store.createRecord('comment', {...formValues, upload}).save();
 
+    reset();
+  }
+
+}
 });
